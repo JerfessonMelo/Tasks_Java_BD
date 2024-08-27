b5e78e8a5eb8a8b71631dbbc4404dc20a6b9e1f9
JerfessonMelo <jerfessonmelo123@gmail.com>
2024-08-27 09:55:47 -0300

    Task Clima Quente Frio com If-else

---
+++ TaskClimaQuenteFrio/Clima.java
@@ -0,0 +1,20 @@
+package TaskClimaQuenteFrio;
+
+public class Clima {
+
+    
+
+    public static void main(String[] args) {
+
+        int tempo = 20;
+        
+        if (tempo > 30) {
+            System.out.println("O Clima esta Quente, Considere usar um protetor solar e tomar uma heineken Geladinha!");
+            
+        } else {
+            System.out.println("O Clima esta Frio, e esta agradevel para um piquinique!");
+        }
+        
+    }
+
+}
