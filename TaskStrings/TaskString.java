7092a5b36cf2e0fa68a02ea9a96928ece04074b1
JerfessonMelo <jerfessonmelo123@gmail.com>
2024-08-27 09:43:30 -0300

    Inicio Task String

---
+++ TaskStrings/TaskString.java
@@ -0,0 +1,23 @@
+package TaskStrings;
+
+public class TaskString {
+
+    public static void main(String[] args) {
+
+
+        String Gasolina = "TaSemGasolina";
+        int stringLength = Gasolina.length();
+        System.out.println(stringLength);
+        String nomeMinusculo = Gasolina.toLowerCase();
+        System.out.println(nomeMinusculo);
+        String nomeMaiusculo = Gasolina.toUpperCase();
+        System.out.println(nomeMaiusculo);
+        
+        
+
+
+
+
+
+    }
+}
