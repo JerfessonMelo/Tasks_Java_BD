0c31eeb478552b68b2b4af98b82eb363e9e7d224
JerfessonMelo <jerfessonmelo123@gmail.com>
2024-08-27 10:05:27 -0300

    Task Dois Valores, Qual Valor e Maior if-eslse

---
+++ TaskDoisValores/DoisValores.java
@@ -0,0 +1,18 @@
+package TaskDoisValores;
+
+public class DoisValores {
+
+    public static void main(String[] args) {
+        
+        int numberOne = 1;
+        int numberTwo = 2;
+
+
+        if (numberOne > numberTwo) {
+            System.out.println("Number One greater than Numbre Two: " + numberOne);            
+        } else {
+            System.out.println("Number Two greater the Numbre One: " + numberTwo);
+        }
+    }
+
+}
