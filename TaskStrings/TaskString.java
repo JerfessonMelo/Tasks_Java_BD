2f57444e0609f8d8121b9ca6a6b1580701c3a9de
JerfessonMelo <jerfessonmelo123@gmail.com>
2024-09-02 21:03:56 -0300

    Contnuação Strings

--- TaskStrings/TaskString.java
+++ TaskStrings/TaskString.java
@@ -5,18 +5,24 @@ public class TaskString {
     public static void main(String[] args) {
 
 
-        String Gasolina = "TaSemGasolina";
-        int stringLength = Gasolina.length();
+        String gasolina = "TaSemGasolina";
+        int stringLength = gasolina.length();
         System.out.println(stringLength);
-        String nomeMinusculo = Gasolina.toLowerCase();
+        String nomeMinusculo = gasolina.toLowerCase();
         System.out.println(nomeMinusculo);
-        String nomeMaiusculo = Gasolina.toUpperCase();
+        String nomeMaiusculo = gasolina.toUpperCase();
         System.out.println(nomeMaiusculo);
-        
-        
 
-
-
+        int vogais = 0;
+        String listaVogais = "aeiouAEIOU";
+        for(int i=0; i <gasolina.length(); i++){
+            char letra = gasolina.charAt(i);
+            if (listaVogais.indexOf(letra)!=-1) {
+                vogais++;
+            }
+        }
+             
+        System.out.println(vogais);
 
 
     }
