57db5cf7b6cde6f0d1ee8673a5d1cbfbedd62c1b
JerfessonMelo <jerfessonmelo123@gmail.com>
2024-09-16 19:57:14 -0300

    TaskPessoas

---
+++ TaskPessoa/Pessoas.java
@@ -0,0 +1,31 @@
+public class Pessoas {
+
+    public static void main(String[] args) {
+     pessoa pessoal = new pessoa();
+
+     pessoal.setNome("Italo");
+     pessoal.setIdade(20);
+     pessoal.setAltura(200);
+     pessoal.setPeso(58);
+     pessoal.setCorOlhos("Azul");
+     pessoal.setCorCabelo("Preto");
+     pessoal.setCorPele("pardo");
+     pessoal.setSexo("homem");
+     pessoal.setNacionalidade("Brasileiro");
+     pessoal.setEstadoCivil("Solteiro");
+
+     pessoal.setNome("Talles");
+     pessoal.setIdade(23);
+     pessoal.setAltura(190);
+     pessoal.setPeso(75);
+     pessoal.setCorOlhos("Preto");
+     pessoal.setCorCabelo("Azul");
+     pessoal.setCorPele("Branco");
+     pessoal.setSexo("homem");
+     pessoal.setNacionalidade("Brasileiro");
+     pessoal.setEstadoCivil("Casaso");
+        
+
+    }
+
+}
---
+++ TaskPessoa/pessoa.java
@@ -0,0 +1,77 @@
+public class pessoa {
+
+	private String nome;
+	private int idade;
+	private double altura;
+	private double peso;
+	private String CorOlhos;
+	private String CorCabelo;
+	private String CorPele;
+	private String sexo;
+	private String nacionalidade;
+	private String estadoCivil;
+	
+	String getNome() {
+		return nome;
+	}
+	void setNome(String Nome) {
+		this.nome = Nome;
+	}
+	int getIdade() {
+		return idade;
+	}
+	void setIdade(int idade) {
+		this.idade = idade;
+	}
+	
+	double setAltura() {
+		return altura;
+	}
+	void setAltura(double altura) {
+		this.altura = altura;
+	}
+	
+	double setPeso() {
+		return peso;
+	}
+	void setPeso(double peso) {
+		this.peso = peso;
+	}
+	String setCorOlhos() {
+		return CorOlhos;
+	}
+	void setCorOlhos(String CorOlhos) {
+		this.CorOlhos = CorOlhos;
+	}
+	String setCorCabelo() {
+		return CorCabelo;
+	}
+	void setCorCabelo(String CorCabelo) {
+		this.CorCabelo = CorCabelo;
+	}
+	String setCorPele() {
+		return CorPele;
+	}
+	void setCorPele(String CorPele) {
+		this.CorPele = CorPele;
+	}
+	String setSexo() {
+		return sexo;
+	}
+	void setSexo(String sexo) {
+		this.sexo = sexo;
+	}
+	String getNacionalidade() {
+		return nacionalidade;
+	}
+	void setNacionalidade(String naciolalidade) {
+		this.nacionalidade = naciolalidade;
+	}
+	String setEstadoCivil() {
+		return estadoCivil;
+	}
+	void setEstadoCivil(String estadoCivil) {
+		this.estadoCivil = estadoCivil;
+	}
+		
+}
